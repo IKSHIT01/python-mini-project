5027498c8773aab7d2bb498da4110ee27b5f916e
IKSHIT01 <153857436+IKSHIT01@users.noreply.github.com>
2026-04-22 19:41:56 +0530

    Update project.sql

--- project.sql
+++ project.sql
@@ -1,8 +1,15 @@
 CREATE TABLE Students (
-    id INT,
+    id INT PRIMARY KEY,
     name VARCHAR(50),
     marks INT
 );
 
 INSERT INTO Students VALUES (1, 'Aman', 85);
+INSERT INTO Students VALUES (2, 'Riya', 90);
+INSERT INTO Students VALUES (3, 'Rahul', 78);
+
 SELECT * FROM Students;
+
+SELECT * FROM Students WHERE marks > 80;
+
+SELECT AVG(marks) AS average_marks FROM Students;
